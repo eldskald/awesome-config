f050bf256002edba856e58c81d165f0800efc1f7
rafael <rafaelbordoni00@gmail.com>
2023-05-31 01:31:19 -0300

    Update key binds and file structure

--- rc.lua
+++ rc.lua
@@ -6,7 +6,7 @@ local awful = require('awful')
 -- Startup system
 local startup = require('startup_apps')
 local dir = gears.filesystem.get_configuration_dir()
-awful.spawn.with_shell('picom --config ' .. dir .. '/picom.conf')
-for _,app in startup do
+awful.spawn.with_shell('picom --config ' .. dir .. '/system/picom.conf')
+for _, app in startup do
   awful.spawn.with_shell(app)
 end
